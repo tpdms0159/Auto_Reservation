2a0b89cef8b50722fa9b7e8c603ef5a608b7e3f6
tpdms0159 <tpdms0159@naver.com>
2024-12-20 12:00:53 +0900

    template

---
+++ src/pages/Main/Index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainPage from './MainPage'
+
+function Index() {
+    return (
+      <MainPage/>
+  )
+}
+
+export default Index
---
+++ src/pages/Reservation/Index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Reservation from "./Reservation";
+
+function Index() {
+  return <Reservation />;
+}
+
+export default Index;
