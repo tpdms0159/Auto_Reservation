e45a2d313d254444d70d01de7e69349c9a61780b
tpdms0159 <tpdms0159@naver.com>
2024-12-20 15:54:21 +0900

    환경 설정

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function MainPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <Headers />;
 }
 
-export default MainPage
+export default MainPage;
---
+++ src/router/router.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+function router() {
+  return <Routes></Routes>;
+}
+
+export default router;
