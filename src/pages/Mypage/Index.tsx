da148cc74e7cc83ae3500ad7d133979396c38ee3
tpdms0159 <tpdms0159@naver.com>
2024-12-22 14:25:55 +0900

    env settings

---
+++ src/pages/Mypage/Index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Mypage from "./Mypage";
+
+function Index() {
+  return <Mypage />;
+}
+
+export default Index;
---
+++ src/pages/User/Index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import User from "./User";
+
+function Index() {
+  return <User />;
+}
+
+export default Index;
