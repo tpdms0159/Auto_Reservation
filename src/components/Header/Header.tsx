e71ba230b09d0cded5793bdea72f090697989b85
tpdms <82717343+tpdms0159@users.noreply.github.com>
2024-12-22 19:19:49 +0900

    Revert "feature login layout"

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import { Logo, Wrapper, UserImg } from './HeaderStyle';
-import userImg from 'assets/user_logo.png';
 
 function Header() {
-  return (
-    <Wrapper>
-      <Logo>AutoeverForever</Logo>
-      <UserImg src={userImg} alt="user" />
-    </Wrapper>
-  );
+  return <div></div>;
 }
 
 export default Header;
--- src/components/Header/HeaderStyle.tsx
+++ src/components/Header/HeaderStyle.tsx
@@ -1,23 +1,8 @@
-import styled from 'styled-components';
+import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  position: sticky;
-
-  width: 100%;
-  background-color: white;
 `;
 
-export const Logo = styled.div`
-  font-size: 3rem;
-  padding: 2rem;
-  color: black;
-`;
-
-export const UserImg = styled.img`
-  width: 3rem;
-  height: 3rem;
-  padding: 2rem;
-`;
