9e0a6dd23a0fb50d85fc4c2a23b189807cf65c1e
tpdms0159 <tpdms0159@naver.com>
2024-12-22 19:12:42 +0900

    feature login layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Logo, Wrapper, UserImg } from './HeaderStyle';
+import userImg from 'assets/user_logo.png';
 
 function Header() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Logo>AutoeverForever</Logo>
+      <UserImg src={userImg} alt="user" />
+    </Wrapper>
+  );
 }
 
 export default Header;
